b14d04daf95f45d01bb044f071bdc8f6a716aafe
Erick Tomaz <71977017+ericktomazdev@users.noreply.github.com>
2021-11-13 22:45:08 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tunico Messaging',
+      title: 'Messaging App',
       theme: themeData(context),
       home: Home(),
     );
